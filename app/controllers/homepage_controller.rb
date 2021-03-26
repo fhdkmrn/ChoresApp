cc7a67a23dcc1a4ee71557171398ae12de51fb03
NekoDerek <noreply@example.com>
2021-03-26 14:12:41 +0300

    Refactor homepage controller

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class HomepageController < ApplicationController
-	def index
-		@user = User.find_by(id: session[:user_id])
-		if @user != nil
-		  redirect_to '/user'
-		end
-		@users = User.order(points: :desc)
-	end
+  def index
+    @user = User.find_by(id: session[:user_id])
+    redirect_to user_path unless @user.nil?
+    @users = User.order(points: :desc)
+  end
 end
