5bbfa64ffb9030239a907165d2a4cf2820dd0733
NekoDerek <noreply@example.com>
2021-03-26 14:14:45 +0300

    Refactor weekly controller

--- app/controllers/weekly_controller.rb
+++ app/controllers/weekly_controller.rb
@@ -1,39 +1,31 @@
+# frozen_string_literal: true
 
 class WeeklyController < ApplicationController
-
-	def index
-		@user = User.find_by(id: session[:user_id])
-		if @user == nil
-		  redirect_to '/login'
-		end
-		@users = User.order(points: :desc)
-		chores = ["sweeping","bathroom","cleaning","wiping","playing","bye","trying","dying"]
-		# For now, uninitialized choreslist
-		if Choreslist.all.count == 0
-			for i in 0..7
-				chore = Choreslist.new
-				chore.choreName = chores[i]
-				chore.taskID = i
-				chore.save
-
-			end
-		end
-		chore = Choreslist.find_by(taskID:@user.choreCycle)
-		chore.user = @user.name
-		chore.save
-		@current_chore = chore
-     	@users = User.paginate(page: params[:page])
-
-	end
-
-    def events
-		@user = User.find_by(id: session[:user_id])
-		if @user == nil
-		  redirect_to '/login'
-		end
+  def index
+    @user = User.find_by(id: session[:user_id])
+    redirect_to login_path if @user.nil?
+    @users = User.order(points: :desc)
+    chores = %w[sweeping bathroom cleaning wiping playing bye trying dying]
+    # For now, uninitialized chores_list
+    if ChoresList.all.count.zero?
+      chore = ChoresList.new
+      (0..7).each do |i|
+        chore.choreName = chores[i]
+        chore.taskID = i
+        chore.save
+      end
     end
+    chore = ChoresList.find_by(taskID: @user.choreCycle)
+    chore.user = @user.name
+    chore.save
+    @current_chore = chore
+    @users = User.paginate(page: params[:page])
+  end
 
+  def events
+    @user = User.find_by(id: session[:user_id])
+    redirect_to login_path if @user.nil?
+  end
 
-	def problems
-	end
+  def problems; end
 end
