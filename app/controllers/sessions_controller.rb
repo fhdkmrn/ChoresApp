0b9a84ed92a482d282869cb9ab2ae8a20a20ff87
NekoDerek <noreply@example.com>
2021-03-26 14:10:47 +0300

    Refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
     @current_user = User.find_by(id: session[:user_id])
-    if @current_user != nil
-      redirect_to '/user'
-    end
+    redirect_to user_path unless @current_user.nil?
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
+    if user&.authenticate(params[:session][:password])
+      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/user'
+      redirect_to user_path
     else
-    # If user's login doesn't work, send them back to the login form.
+      # If user's login doesn't work, send them back to the login form.
       flash[:danger] = 'Invalid email/password combination'
-      redirect_to '/login'
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to root_path
   end
 end
